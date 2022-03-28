76dca0da744b0faaf24c87ca34c4396d5f3260fb
wetech16 <67137441+wetech16@users.noreply.github.com>
2022-03-27 19:32:38 -0700

    k

--- carousel-movie/index.js
+++ carousel-movie/index.js
@@ -1,99 +1,125 @@
-// CONST
-const domSeletors = {
-    'todolistHeader': '.todolist__header',
-    'todolistContent': '.todolist__content',
-}
+const MovieAPI = (function () {
+  function getMovies() {
+    return fetch("http://localhost:3000/movies").then((res) =>
+      res.json()
+    );
+  }
+  return { getMovies };
+})();
 
-
-// Data
-const title = "My To Do List";
-const submitText = "Add";
-let todos = [{
-    "userId": 1,
-    "id": 1,
-    "title": "Hit the gym",
-    "completed": false
-}, {
-    "userId": 1,
-    "id": 2,
-    "title": "sdfasf aut autem",
-    "completed": false
-}, {
-    "userId": 1,
-    "id": 3,
-    "title": "sdfsafsadf aut autem",
-    "completed": true
-}, {
-    "userId": 1,
-    "id": 4,
-    "title": "dfdsfsafasfs aut autem",
-    "completed": false
-}]
-
-
-function deletTodo(id) {
-    todos = todos.filter(todo => todo.id !== id);
-}
-
-function generateHeaderContent(title, submitText) {
-    return `<h1 class="todolist__header__title">${title}</h1>
-    <div class="input-bar">
-        <input class="input-bar__input"/>
-        <button class="input-bar__submit">${submitText}</button>
-    </div>`
-}
-
-
-function generateTodoItem(todo) {
-    return `<li id="todo-${todo.id}" class="todolist__content__row">
-    <span class="todolist__content__item">${todo.title}</span>
-    <button  class="todolist__content__action" >X</button>
-</li>`
-}
-
-function generateTodoList(todos) {
-    return todos.map(todo => generateTodoItem(todo)).join('')
-}
-
-
-function renderHeader(title, submitText) {
-    const ele = document.querySelector(domSeletors.todolistHeader)
-    const tmp = generateHeaderContent(title, submitText)
-    render(ele, tmp)
-}
-
-function renderTodoList(todos) {
-    const tmp = generateTodoList(todos);
-    const ele = document.querySelector(domSeletors.todolistContent);
-    render(ele, tmp)
-}
-
-function render(element, template) {
+const View = (function () {
+  const domSelectors = {
+    movieCardContainer: ".movie__card__container",
+    movieCard: ".movie__card",
+    cardBtnLeft: ".card__btn-left",
+    cardBtnRight: ".card__btn-right",
+  };
+  function generateMovieCardContent({ imgUrl, name, outlineInfo }) {
+    return `<div class="movie__card">
+        <img src="${imgUrl}" alt="" />
+        <h2 class="movie__card__title">Movie: ${name}</h2>
+        <p class="move__card__info">info: ${outlineInfo}
+    </div>`;
+  }
+  function generateMovieCards(movies) {
+    return movies
+      .map((movie) => generateMovieCardContent(movie))
+      .join("");
+  }
+  function renderMoveCards(movies) {
+    const temp = generateMovieCards(movies);
+    const ele = document.querySelector(
+      domSelectors.movieCardContainer
+    );
+    render(ele, temp);
+  }
+  function render(element, template) {
     element.innerHTML = template;
-}
+  }
+  return {
+    renderMoveCards,
+    domSelectors,
+  };
+})();
 
-function setUpEvent() {
-    document.querySelector(domSeletors.todolistContent).addEventListener('click', (e) => {
-        if (isDeleteButton(e.target)) {
-            const id = getTodoIdFromBtn(e.target);
-            deletTodo(id);
-            renderTodoList(todos);
-        }
-    })
-}
+const Model = (function (movieAPI) {
+  return { movieAPI };
+})(MovieAPI);
 
-function getTodoIdFromBtn(btnElement) {
-    return +(btnElement.parentElement.id.substring(5))
-}
+const Controller = (function (view, model) {
+  class State {
+    constructor() {
+      this._movies = [];
+      this.start = 0;
+      this.end = this.start + 4;
+    }
+    get movies() {
+      return this._movies;
+    }
+    set movies(newMovies) {
+      this._movies = newMovies;
+      view.renderMoveCards(this._movies);
+    }
+  }
+  let state = new State();
 
-function isDeleteButton(element) {
-    return element.classList.contains('todolist__content__action')
-}
+  function display4Cards() {
+    //btn display
+    const btnLeft = document.querySelector(
+      view.domSelectors.cardBtnLeft
+    );
+    const btnRight = document.querySelector(
+      view.domSelectors.cardBtnRight
+    );
+    if (state.start == 0) {
+      btnLeft.style.display = "none";
+    } else {
+      btnLeft.style.display = "flex";
+    }
+    if (state.end == state.movies.length) {
+      btnRight.style.display = "none";
+    } else {
+      btnRight.style.display = "flex";
+    }
 
+    // card display
+    const ele = document.querySelector(
+      view.domSelectors.movieCardContainer
+    );
+    let l = state.movies.length;
+    for (let i = 0; i < l; i++) {
+      ele.childNodes[i].style.display = "none";
+    }
+    for (let i = state.start; i < state.end; i++) {
+      ele.childNodes[i].style.display = "block";
+    }
+  }
+  function setUpEvent() {
+    document
+      .querySelector(view.domSelectors.cardBtnLeft)
+      .addEventListener("click", (e) => {
+        state.start--;
+        state.end--;
+        display4Cards();
+      });
+    document
+      .querySelector(view.domSelectors.cardBtnRight)
+      .addEventListener("click", (e) => {
+        state.start++;
+        state.end++;
+        display4Cards();
+      });
+  }
+  function init() {
+    model.movieAPI.getMovies().then((movieData) => {
+      state.movies = movieData;
+      display4Cards();
+      setUpEvent();
+    });
+  }
+  return {
+    init,
+  };
+})(View, Model);
 
-// init
-renderHeader(title, submitText);
-renderTodoList(todos);
-
-// init Event
-setUpEvent()
+Controller.init();
